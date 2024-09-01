dd578158b1a416a4c4404cec9956111bc6c9e4e2
GautamGulati28 <gautamgulati2809@gmail.com>
2024-09-01 09:53:51 +0530

    updated logic script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,70 @@
 
-const socket = io()
+// const socket = io()
+
+// if (navigator.geolocation) {
+
+//     navigator.geolocation.watchPosition(
+//         (position) => {
+//             const { latitude, longitude } = position.coords;
+
+//             socket.emit("send-location", { latitude, longitude });
+//         },
+//         (err) => console.log(err),
+//         {
+//             enableHighAccuracy: true,
+//             maximumAge: 0,
+//             timeout: 5000
+//         }
+//     )
+
+// } else {
+//     alert("Please give location permission to this web.")
+// }
+
+
+
+// var map = L.map("map").setView([0, 0], 10);
+
+
+// L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map)
+
+
+
+
+// const marker = {}
+
+// socket.on("recieve-location", (data) => {
+//     const { id, latitude, longitude } = data;
+
+//     map.setView([latitude, longitude],16);
+
+//     if (marker[id]) {
+//         marker[id].setLatLng([latitude, longitude]);
+//     }
+//     else {
+//         marker[id] = L.marker([latitude, longitude]).addTo(map);
+//     }
+// })
+
+
+
+
+
+// socket.on("user-disconnected", (id) => {
+//     if(marker[id]){
+//         map.removeLayer(marker[id])
+//         delete marker[id]
+//     }
+    
+
+// })
+
+const socket = io();
 
 if (navigator.geolocation) {
-
     navigator.geolocation.watchPosition(
         (position) => {
             const { latitude, longitude } = position.coords;
-
             socket.emit("send-location", { latitude, longitude });
         },
         (err) => console.log(err),
@@ -15,46 +73,36 @@ if (navigator.geolocation) {
             maximumAge: 0,
             timeout: 5000
         }
-    )
-
+    );
 } else {
-    alert("Please give location permission to this web.")
+    alert("Please give location permission to this web.");
 }
-
-
 
 var map = L.map("map").setView([0, 0], 10);
 
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map)
-
-
-
-
-const marker = {}
+const marker = {};
+let isFirstUpdate = true;  // Flag to check if it's the first location update
 
 socket.on("recieve-location", (data) => {
     const { id, latitude, longitude } = data;
 
-    map.setView([latitude, longitude],16);
+    if (isFirstUpdate) {
+        map.setView([latitude, longitude], 16);
+        isFirstUpdate = false;  // Set the flag to false after the first update
+    }
 
     if (marker[id]) {
         marker[id].setLatLng([latitude, longitude]);
-    }
-    else {
+    } else {
         marker[id] = L.marker([latitude, longitude]).addTo(map);
     }
-})
-
-
-
-
+});
 
 socket.on("user-disconnected", (id) => {
-    if(marker[id]){
-        map.removeLayer(marker[id])
-        delete marker[id]
+    if (marker[id]) {
+        map.removeLayer(marker[id]);
+        delete marker[id];
     }
-    
-
-})
+});
